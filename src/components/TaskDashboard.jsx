c73cdab571eff8e4a31a57c29ec0a664830601f2
espadv69 <aitorjob69@gmail.com>
2025-01-12 14:14:33 +0100

    Add task addition functionality to TaskDashboard component

--- src/components/TaskDashboard.jsx
+++ src/components/TaskDashboard.jsx
@@ -4,6 +4,7 @@ export const TaskDashboard = () => {
   const [tasks, setTasks] = useState([]) // State for tasks
   const [filter, setFilter] = useState('all') // State for filter
 
+  // Load tasks from localStorage
   useEffect(() => {
     const savedTasks = JSON.parse(localStorage.getItem('tasks'))
     if (savedTasks) {
@@ -15,6 +16,16 @@ export const TaskDashboard = () => {
   useEffect(() => {
     localStorage.setItem('tasks', JSON.stringify(tasks))
   }, [tasks])
+
+  // Function to add a task
+  const addTask = (taskName) => {
+    const newTask = {
+      id: Date.now(),
+      name: taskName,
+      completed: false,
+    }
+    setTasks((prevTasks) => [...prevTasks, newTask])
+  }
 
   return (
     <div>
